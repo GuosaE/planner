6726a02954027ed8ceaaf9196b4a070d5e18377d
Guosa_Edebiri <gedebiri@gmail.com>
2021-12-03 07:44:56 -0600

    updated Home component

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return(
+        <div>
+            <br></br>
+            <br></br>
+            <h1 id="welcome">Welcome to your Weekly Planner!</h1>
+        </div>
+    )
+}
+
+export default Home;
