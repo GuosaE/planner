af633a3d92937ac41a5591397e5ee3bd4c96a204
Guosa_Edebiri <gedebiri@gmail.com>
2021-12-03 06:36:58 -0600

    seeded database, took care of CORS issues

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Event.create(day: "Monday", location: "The Office", name: "Super Important Meeting", importance: "High", category: "Business")
+Event.create(day: "Tuesday", location: "Home", name: "Movie Night", importance: "Low", category: "Recreation")
+Event.create(day: "Wednesday", location: "Fancy Restaurant", name: "Anniversary Dinner", importance: "High", category: "Recreation")
+Event.create(day: "Thursday", location: "Art Studio", name: "Finish New Painting", importance: "Medium", category: "Creative")
+Event.create(day: "Friday", location: "Book Club", name: "Book Discussion", importance: "Low", category: "Recreation")
+Event.create(day: "Saturday", location: "Local Mountain", name: "Climb Nearest Mountain", importance: "Medium", category: "Recreation")
+Event.create(day: "Sunday", location: "Bank", name: "Deposit Money", importance: "High", category: "Business")
