c309972bb5c375c5b8f37e1ae8d409d965f4fc69
Guosa_Edebiri <gedebiri@gmail.com>
2021-12-03 07:49:02 -0600

    updated EventsHeader js

---
+++ frontend/src/components/EventsHeader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const EventsHeader = () => {
+
+    return(
+    <div>
+        <div className="eventBanner"> 
+        <h2><u>Activities This Week</u></h2>
+        </div> 
+        <br></br>
+    </div>
+    )
+}
+
+export default EventsHeader;
